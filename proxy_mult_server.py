e415f1b8f45eeb2801c22025dcbe96847fc1a585
Jionghao Chen <jionghaochen@Jionghaos-MacBook-Air.local>
2021-09-14 20:08:48 -0600

    struggling

--- proxy_mult_server.py
+++ proxy_mult_server.py
@@ -1,11 +1,15 @@
 import socket, sys, time
 from multiprocessing import Process
 
+def hand_proxy_network():
 
-def handle_user_proxy(connection, client_sent_data):
+
+
+def handle_user_proxy(address, connection, client_sent_data):
     data = connection.recv(10240)
+   
     connection.sendall(client_sent_data)
-    s1.close()
+    
 
 
 def proxy_network(HOST, POST):
@@ -28,12 +32,33 @@ def user_proxy(HOST, PORT):
     while True:
         connection, address = s1.accept()
         client_sent_data = proxy_network(data.decode(), 80)
-        p = Process(target=client_sent_data,
-                    args=(connection, client_sent_data),
+        p = Process(target = handle_user_proxy,
+                    args=(address, connection, client_sent_data),
                     daemon=True)
         p.start()
-    s1.close()
+        
 
 
 if __name__ == '__main__':
-    user_proxy('localhost', 8001)
+    #user_proxy('localhost', 8001)
+
+
+
+    HOST = 'localhost'
+    PORT = 8001
+    net_PORT = 80
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+        s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s1.bind((HOST, PORT))
+        s1.listen(5)
+
+        while True:
+            connection, address = s1.accept()
+            net_HOST = connection.recv(10240)
+            ip_address = socket.gethostbyname(net_HOST)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+                s2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s2.connect((ip_address, net_PORT))
+                s2.sendall(net_HOST)
+                data = s2.recv(10240)
+                s2.shutdown(socket.SHUT_RDWR)
