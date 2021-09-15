73d23f7ce62b6d6c2f5b7da23732f98eac34cde3
JohnChen97 <chenjionghao@yahoo.com>
2021-09-15 15:23:32 -0600

    CHange the file name

--- proxy_client.py
+++ proxy_client.py
@@ -1,11 +1,23 @@
 import socket, sys, time
+from multiprocessing import Pool
 
-if __name__ == '__main__':
+
+def client_connection(address):
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.connect(('localhost', 8001))
+    s.connect(address)
     s.sendall(b'GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n')
     #s.shutdown(socket.SHUT_RDWR)
     data = s.recv(20480)
-    sys.stdout.write(repr(data))
+    sys.stdout.write(repr(data) + '\n')
     s.close()
+
+
+if __name__ == '__main__':
+    address = [('localhost', 8001)]
+    #p = Pool()
+    #p.map(client_connection, address * 10)
+
+    with Pool() as pool:
+        pool.map(client_connection, address * 5)
--- proxy_server.py
+++ proxy_server.py
@@ -1,4 +1,17 @@
 import socket, sys, time
+from multiprocessing import Process
+
+
+def processing(connection, user_data, s2):
+    s2.sendall(user_data)
+    data = s2.recv(10240)
+    connection.sendall(data)
+
+
+def handle_user_proxy():
+    data = connection.recv(10240)
+
+    connection.sendall(client_sent_data)
 
 
 def proxy_network(HOST, POST):
@@ -20,12 +33,39 @@ def user_proxy(HOST, PORT):
     s1.listen(5)
     while True:
         connection, address = s1.accept()
-        data = connection.recv(10240)
-        if not data:
-            break
-        connection.sendall(proxy_network(data.decode(), 80))
-    s1.close()
+        client_sent_data = proxy_network(data.decode(), 80)
+        p = Process(target=handle_user_proxy,
+                    args=(address, connection, client_sent_data),
+                    daemon=True)
+        p.start()
 
 
 if __name__ == '__main__':
-    user_proxy('localhost', 8001)
+    #user_proxy('localhost', 8001)
+
+    HOST = 'localhost'
+    PORT = 8001
+    net_HOST = 'www.google.com'
+    net_PORT = 80
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s1:
+        s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s1.bind((HOST, PORT))
+        s1.listen(5)
+
+        while True:
+            connection, address = s1.accept()
+            user_data = connection.recv(10240)
+            ip_address = socket.gethostbyname(net_HOST)
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s2:
+                s2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s2.connect((ip_address, net_PORT))
+
+                p = Process(target=processing,
+                            args=(connection, user_data, s2),
+                            daemon=True)
+                p.start()
+
+        s2.shutdown(socket.SHUT_RDWR)
+        s2.close()
+        s1.shutdown(socket.SHUT_RDWR)
+        s1.close()
